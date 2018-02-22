f4d0a704801a2624c260b85fb8e004853c27c45a
Bogdan Pintea <bpintea@gmail.com>
2018-02-22 18:53:58 +0100

    add escape portion in statement for SQLColumns

--- driver/catalogue.c
+++ driver/catalogue.c
@@ -55,17 +55,22 @@ SQLRETURN EsSQLTablesW(
 	SQLTCHAR *table_type, *table_name;
 	size_t count, pos;
 
+	if (stmt->metadata_id == SQL_TRUE)
+		FIXME; // FIXME
+
+	/* TODO: server support needed for cat. & sch. name filtering */
+
 	if (CatalogName && (wmemcmp(CatalogName, MK_TSTR(SQL_ALL_CATALOGS), 
 					NameLength1) != 0)) {
-		ERR("filtering by catalog is not supported."); /* TODO? */
-		goto empty;
+		ERR("filtering by catalog is not supported.");
+		RET_HDIAG(stmt, SQL_STATE_IM001, "catalog filtering not supported", 0);
 	}
 	if (SchemaName && (wmemcmp(SchemaName, MK_TSTR(SQL_ALL_SCHEMAS), 
 					NameLength2) != 0)) {
 		ERR("filtering by schemas is not supported.");
-		goto empty;
+		RET_HDIAG(stmt, SQL_STATE_IM001, "schema filtering not supported", 0);
 	}
-	if (TableType) { /* TODO: server support needed */
+	if (TableType) {
 		table_type = _wcsupr(TableType);
 		if ((! wcsstr(table_type, MK_TSTR("TABLE"))) && 
 				(! wcsstr(table_type, MK_TSTR("ALIAS")))) {
@@ -135,16 +140,22 @@ SQLRETURN EsSQLColumnsW
 	int clen;
 	SQLRETURN ret;
 
+	if (stmt->metadata_id == SQL_TRUE)
+		FIXME; // FIXME
+	
+	/* TODO: server support needed for cat. & sch. name filtering */
+
 	if (szCatalogName && (wmemcmp(szCatalogName, MK_TSTR(SQL_ALL_CATALOGS), 
 					cchCatalogName) != 0)) {
 		ERR("filtering by catalog is not supported."); /* TODO? */
-		goto empty;
+		RET_HDIAG(stmt, SQL_STATE_IM001, "catalog filtering not supported", 0);
 	}
 	if (szSchemaName && (wmemcmp(szSchemaName, MK_TSTR(SQL_ALL_SCHEMAS), 
 					cchSchemaName) != 0)) {
 		ERR("filtering by schemas is not supported.");
-		goto empty;
+		RET_HDIAG(stmt, SQL_STATE_IM001, "schema filtering not supported", 0);
 	}
+
 	if (szTableName && cchTableName) {
 		if (ESODBC_MAX_IDENTIFIER_LEN < cchTableName) {
 			ERR("TableName `" LTPDL "` too long (limit: %zd).", cchTableName,
@@ -171,8 +182,10 @@ SQLRETURN EsSQLColumnsW
 	}
 
 	clen = swprintf(wbuf, sizeof(wbuf)/sizeof(SQLTCHAR), 
-			MK_TSTR("%s '%.*s' '%.*s'"), MK_TSTR(ESODBC_SQL_COLUMNS), tn_clen,
-			tablename, cn_clen, columnname);
+			MK_TSTR("%s '%.*s' ESCAPE '" ESODBC_PATTERN_ESCAPE "' "
+				"'%.*s' ESCAPE '" ESODBC_PATTERN_ESCAPE "'"), 
+			MK_TSTR(ESODBC_SQL_COLUMNS), 
+			tn_clen, tablename, cn_clen, columnname);
 	if (clen <= 0 || sizeof(wbuf)/sizeof(SQLTCHAR) <= clen) { /* == */
 		ERRN("SQL printing failed (buffer too small (%zdB)?).", sizeof(wbuf));
 		RET_HDIAGS(stmt, SQL_STATE_HY000);
@@ -184,10 +197,6 @@ SQLRETURN EsSQLColumnsW
 	if (SQL_SUCCEEDED(ret))
 		ret = post_statement(stmt);
 	return ret;
-
-empty:
-	RET_HDIAG(stmt, SQL_STATE_HYC00, "Table filtering not supported", 0);
-	// FIXME: add support for it
 }
 
 SQLRETURN EsSQLSpecialColumnsW
