1941196afe22f94d8e74f39ea880556067c7cb5f
Bogdan Pintea <bogdan.pintea@elastic.co>
2018-05-16 11:27:03 +0200

    b/f: use the wide chars variants in switch
    the value stays the same, but this is the technical right way

--- driver/util.c
+++ driver/util.c
@@ -30,7 +30,7 @@ BOOL wstr2long(wstr_st *val, long *out)
 		return FALSE;
 
 	switch (val->str[0]) {
-		case '-':
+		case L'-':
 			negative = TRUE;
 			i ++;
 			break;
@@ -44,9 +44,9 @@ BOOL wstr2long(wstr_st *val, long *out)
 
 	for ( ; i < val->cnt; i ++) {
 		/* is it a number? */
-		if (val->str[i] < '0' || '9' < val->str[i])
+		if (val->str[i] < L'0' || L'9' < val->str[i])
 			return FALSE;
-		digit = val->str[i] - '0';
+		digit = val->str[i] - L'0';
 		/* would it overflow?*/
 		if (LONG_MAX - res < digit)
 			return FALSE;
