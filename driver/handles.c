1cbb53af7a6596e235ceabf64903ffd94019b520
Bogdan Pintea <bogdan.pintea@elastic.co>
2018-06-20 13:58:45 +0200

    b/f: (re)fix logging descriptor
    SQLLEN is defined as int64

--- driver/handles.c
+++ driver/handles.c
@@ -2275,7 +2275,7 @@ SQLRETURN EsSQLSetDescFieldW(
 					(SQLLEN)(intptr_t)ValuePtr);
 			/* rec field's type is signed :/; a negative is dangerous l8r  */
 			if ((SQLLEN)(intptr_t)ValuePtr < 0) {
-				ERRH(desc, "octet length attribute can't be negative (%ld)",
+				ERRH(desc, "octet length attribute can't be negative (%lld)",
 						(SQLLEN)(intptr_t)ValuePtr);
 				RET_HDIAGS(desc, SQL_STATE_HY000);
 			}
