507971e7313ddc494a673d309da730ed289bf347
Bogdan Pintea <bpintea@gmail.com>
2018-09-17 23:20:43 +0200

    fixed attach_error tests

--- test/test_queries.cc
+++ test/test_queries.cc
@@ -8,12 +8,13 @@ extern "C" {
 #include "queries.h"
 } // extern C
 
+#include "connected_dbc.h"
 #include <gtest/gtest.h>
 
 
 namespace test {
 
-class Queries : public ::testing::Test {
+class Queries : public ::testing::Test, public ConnectedDBC {
 };
 
 TEST_F(Queries, attach_error_sql) {
@@ -32,24 +33,27 @@ TEST_F(Queries, attach_error_sql) {
         \"reason\": \"" SRC_AID2 "\"\
       }\
     ],\
-    \"type\": \"parsing_exception\",\
-    \"reason\": \"line 1:8: identifiers must not start with a digit; please use double quotes\"\
+    \"type\": \"" SRC_AID1 "\",\
+    \"reason\": \"" SRC_AID2 "\"\
   },\
   \"status\": " STR(SRC_AID3) "\
 }"
 
-	esodbc_stmt_st stmt;
 	SQLRETURN ret;
 	cstr_st body = CSTR_INIT(SRC_STR);
+	SQLWCHAR *pos, *prev;
 
-	memset(&stmt, 0, sizeof(stmt));
-	ret = attach_error(&stmt, &body, 400);
+	ret = attach_error(stmt, &body, 400);
 	ASSERT_EQ(ret, SQL_ERROR);
 
-	ASSERT_EQ(stmt.hdr.diag.state, SQL_STATE_HY000);
-	ASSERT_STREQ((wchar_t *)stmt.hdr.diag.text,
-			(wchar_t *)MK_WPTR(SRC_AID1 ": " SRC_AID2));
-	ASSERT_EQ(stmt.hdr.diag.native_code, SRC_AID3);
+	ASSERT_EQ(HDRH(stmt)->diag.state, SQL_STATE_HY000);
+	/* skip over ODBC's error message format prefix */
+	for (pos = HDRH(stmt)->diag.text; pos; pos = wcschr(prev, L']')) {
+		prev = pos + 1;
+	}
+	ASSERT_TRUE(prev != NULL);
+	ASSERT_STREQ((wchar_t *)prev, (wchar_t *)MK_WPTR(SRC_AID1 ": " SRC_AID2));
+	ASSERT_EQ(HDRH(stmt)->diag.native_code, SRC_AID3);
 }
 
 TEST_F(Queries, attach_error_non_sql) {
@@ -63,30 +67,28 @@ TEST_F(Queries, attach_error_non_sql) {
 #define SRC_STR	\
 "{\
   \"error\": {\
-    \"root_cause\": [\
-      {\
-        \"tyXXXpe\": \"" SRC_AID1 "\",\
-        \"reason\": \"" SRC_AID2 "\"\
-      }\
-    ],\
-    \"type\": \"parsing_exception\",\
-    \"reason\": \"line 1:8: identifiers must not start with a digit; please use double quotes\"\
+    \"root_cause\": \"" SRC_AID1 "\", \
+    \"tyXXXpe\": \"" SRC_AID1 "\",\
+    \"reason\": \"" SRC_AID2 "\"\
   },\
   \"status\": " STR(SRC_AID3) "\
 }"
 
-	esodbc_stmt_st stmt;
 	SQLRETURN ret;
 	cstr_st body = CSTR_INIT(SRC_STR);
+	SQLWCHAR *pos, *prev;
 
-	memset(&stmt, 0, sizeof(stmt));
-	ret = attach_error(&stmt, &body, 400);
+	ret = attach_error(stmt, &body, 400);
 	ASSERT_EQ(ret, SQL_ERROR);
 
-	ASSERT_EQ(stmt.hdr.diag.state, SQL_STATE_08S01);
-	ASSERT_STREQ((wchar_t *)stmt.hdr.diag.text,
-			(wchar_t *)MK_WPTR(SRC_STR));
-	ASSERT_EQ(stmt.hdr.diag.native_code, SRC_AID3);
+	ASSERT_EQ(HDRH(stmt)->diag.state, SQL_STATE_08S01);
+	/* skip over ODBC's error message format prefix */
+	for (pos = HDRH(stmt)->diag.text; pos; pos = wcschr(prev, L']')) {
+		prev = pos + 1;
+	}
+	ASSERT_TRUE(prev != NULL);
+	ASSERT_STREQ(prev, (wchar_t *)MK_WPTR(SRC_STR));
+	ASSERT_EQ(HDRH(stmt)->diag.native_code, SRC_AID3);
 }
 
 
