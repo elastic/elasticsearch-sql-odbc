aabc7b6caccdfcba13f94430ff7e8051b9dc82b2
Bogdan Pintea <bpintea@gmail.com>
2018-12-04 21:20:10 +0100

    installer: only bootstrap packet if not cleaning
    Top-level 'clean' invokes now installer's 'clean'; intaller's
    build.bat clean would attempt to bootstrap 'packet', which should not
    really be needed since:
    - this had been done already, presumably on the recently done build that
    needs cleaning;
    - cleaning should not require an up-to-date 'packet'.
    
    Bootstrapping takes time (network connects), inconvenient in driver's
    development life-cycle.

--- installer/build.bat
+++ installer/build.bat
@@ -2,15 +2,17 @@
 
 cd %~dp0
 
-.paket\paket.bootstrapper.exe
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+IF NOT [%1] == [clean] (
+	.paket\paket.bootstrapper.exe
+	IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-IF EXIST paket.lock (
-	.paket\paket.exe restore
-	IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-) ELSE (
-	.paket\paket.exe install
-	IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+	IF EXIST paket.lock (
+		.paket\paket.exe restore
+		IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+	) ELSE (
+		.paket\paket.exe install
+		IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+	)
 )
 
 "packages\build\FAKE.x64\tools\FAKE.exe" "build\\scripts\\Targets.fsx" "cmdline=%*"
