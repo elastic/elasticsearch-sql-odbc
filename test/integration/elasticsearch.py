8fe617ce6fa5b942ab8983a3cf61b5c1cc8e90e8
Bogdan Pintea <bpintea@gmail.com>
2019-02-19 11:02:00 +0100

    Revert "catch and ignore exceptions on ES shutting down"
    This reverts commit bb32d8f972b976f014111d4da454a52fbed5e915.

--- test/integration/elasticsearch.py
+++ test/integration/elasticsearch.py
@@ -112,18 +112,15 @@ class Elasticsearch(object):
 
 	@staticmethod
 	def _stop_es(es_proc):
-		try:
-			children = es_proc.children()
-			children.append(es_proc)
-			for child in children:
-				child.terminate()
-			gone, alive = psutil.wait_procs(children, timeout=Elasticsearch.TERM_TIMEOUT)
-			for child in alive:
-				child.kill()
+		children = es_proc.children()
+		children.append(es_proc)
+		for child in children:
+			child.terminate()
+		gone, alive = psutil.wait_procs(children, timeout=Elasticsearch.TERM_TIMEOUT)
+		for child in alive:
+			child.kill()
 
-			print("Elasticsearch stopped.")
-		except Exception as e:
-			print("ERROR: failed to stop Elasticsearch cleanly: %s" % e)
+		print("Elasticsearch stopped.")
 
 	def _start_elasticsearch(self, es_dir):
 		start_script = os.path.join(es_dir, "bin", "elasticsearch")
