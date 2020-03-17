f5caffcc6d9a3fb9bf6fbe320613b5980a4e6b6e
Bogdan Pintea <bogdan.pintea@elastic.co>
2020-03-17 23:17:02 +0100

    Fixes: possible buffer overrun on interval conversion. Catalog length handling (#217)
    * fix overrun when converting a c-string to interval
    
    When converting a C-string to a wide string, the conversion function
    0-terminates the output. However, the allocated destination buffer
    lacked the space for the terminator. This commit fixes that.
    
    * fix catalog setting
    
    The API function provides the byte count for the wide string name of the
    catalog, not the character count, as so far implemented. This commit
    fixes the lenght handling.
    
    (cherry picked from commit e39037555ccd2fc1dc52179419c7580e2c20bf51)

--- driver/connect.c
+++ driver/connect.c
@@ -3127,7 +3127,7 @@ static SQLRETURN check_catalog_name(esodbc_dbc_st *dbc, SQLWCHAR *name,
 	if (len < 0) {
 		catalog.cnt = wcslen(name);
 	} else {
-		catalog.cnt = (size_t)len;
+		catalog.cnt = ((size_t)len)/sizeof(SQLWCHAR);
 	}
 	if (! EQ_WSTR(&dbc->catalog, &catalog)) {
 		if (! dbc->catalog.cnt) {
@@ -3281,7 +3281,7 @@ SQLRETURN EsSQLSetConnectAttrW(
 		case SQL_ATTR_CURRENT_CATALOG:
 			INFOH(dbc, "setting current catalog to: `" LWPDL "`.",
 				/* string should be 0-term'd */
-				0 <= StringLength ? StringLength : SHRT_MAX,
+				0 <= StringLength ? StringLength/sizeof(SQLWCHAR) : SHRT_MAX,
 				(SQLWCHAR *)Value);
 			return check_catalog_name(dbc, (SQLWCHAR *)Value, StringLength);
 
