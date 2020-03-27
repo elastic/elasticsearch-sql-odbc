d73066ce72c1faa119f890cd6f94512489fa5d4c
Bogdan Pintea <bogdan.pintea@elastic.co>
2020-03-27 14:50:30 +0100

    Fix varchar type definition (#232)
    * Fix ES varchar types mappings to ODBC types
    
    This commit fixes the mappings of the varchar types to the ODBC data
    types:
    - the KEYWORD, CONSTANT_KEYWORD and TEXT types are now SQL_WVARCHAR,
    which allows certain applications to correctly read the UTF8 content.
    - IP and the other string types (like GEO ones) remain to the previous
    SQL_VARCHAR types.
    
    The handling of double types has also been fixed to reflect the
    correction in Elasticsearch/SQL that now maps DOUBLE and SCALED_FLOAT to
    the same type (SQL_DOUBLE).
    
    * Supplement tests for the varchar type change
    
    add tests querying for SQL source type.
    
    * update types defs for unit test
    
    update the definition of the ES/SQL types used in unit tests.
    
    (cherry picked from commit 45a82b18b516a7065f766988dd7e6317053029c5)

--- test/connected_dbc.h
+++ test/connected_dbc.h
@@ -41,63 +41,68 @@ extern "C" {
  */
 #define SYSTYPES_ANSWER "\
 {\
-	\"columns\":[\
-		{\"name\":\"TYPE_NAME\",\"type\":\"keyword\",\"display_size\":32766},\
-		{\"name\":\"DATA_TYPE\",\"type\":\"integer\",\"display_size\":11},\
-		{\"name\":\"PRECISION\",\"type\":\"integer\",\"display_size\":11},\
-		{\"name\":\"LITERAL_PREFIX\",\"type\":\"keyword\",\"display_size\":32766},\
-		{\"name\":\"LITERAL_SUFFIX\",\"type\":\"keyword\",\"display_size\":32766},\
-		{\"name\":\"CREATE_PARAMS\",\"type\":\"keyword\",\"display_size\":32766},\
-		{\"name\":\"NULLABLE\",\"type\":\"short\",\"display_size\":6},\
-		{\"name\":\"CASE_SENSITIVE\",\"type\":\"boolean\",\"display_size\":1},\
-		{\"name\":\"SEARCHABLE\",\"type\":\"short\",\"display_size\":6},\
-		{\"name\":\"UNSIGNED_ATTRIBUTE\",\"type\":\"boolean\",\"display_size\":1},\
-		{\"name\":\"FIXED_PREC_SCALE\",\"type\":\"boolean\",\"display_size\":1},\
-		{\"name\":\"AUTO_INCREMENT\",\"type\":\"boolean\",\"display_size\":1},\
-		{\"name\":\"LOCAL_TYPE_NAME\",\"type\":\"keyword\",\"display_size\":32766},\
-		{\"name\":\"MINIMUM_SCALE\",\"type\":\"short\",\"display_size\":6},\
-		{\"name\":\"MAXIMUM_SCALE\",\"type\":\"short\",\"display_size\":6},\
-		{\"name\":\"SQL_DATA_TYPE\",\"type\":\"integer\",\"display_size\":11},\
-		{\"name\":\"SQL_DATETIME_SUB\",\"type\":\"integer\",\"display_size\":11},\
-		{\"name\":\"NUM_PREC_RADIX\",\"type\":\"integer\",\"display_size\":11},\
-		{\"name\":\"INTERVAL_PRECISION\",\"type\":\"integer\",\"display_size\":11}\
-	],\
-	\"rows\":[\
-		[\"BYTE\",-6,3,\"'\",\"'\",null,2,false,3,false,false,false,null,0,0,-6,0,10,null],\
-		[\"LONG\",-5,19,\"'\",\"'\",null,2,false,3,false,false,false,null,0,0,-5,0,10,null],\
-		[\"BINARY\",-2,2147483647,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,-3,0,null,null],\
-		[\"NULL\",0,0,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,0,0,null,null],\
-		[\"INTEGER\",4,10,\"'\",\"'\",null,2,false,3,false,false,false,null,0,0,4,0,10,null],\
-		[\"SHORT\",5,5,\"'\",\"'\",null,2,false,3,false,false,false,null,0,0,5,0,10,null],\
-		[\"HALF_FLOAT\",6,3,\"'\",\"'\",null,2,false,3,false,false,false,null,3,3,6,0,2,null],\
-		[\"FLOAT\",7,7,\"'\",\"'\",null,2,false,3,false,false,false,null,7,7,7,0,2,null],\
-		[\"DOUBLE\",8,15,\"'\",\"'\",null,2,false,3,false,false,false,null,15,15,8,0,2,null],\
-		[\"SCALED_FLOAT\",8,15,\"'\",\"'\",null,2,false,3,false,false,false,null,15,15,8,0,2,null],\
-		[\"KEYWORD\",12,32766,\"'\",\"'\",null,2,true,3,true,false,false,null,null,null,12,0,null,null],\
-		[\"TEXT\",12,2147483647,\"'\",\"'\",null,2,true,3,true,false,false,null,null,null,12,0,null,null],\
-		[\"IP\",12,0,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,12,0,null,null],\
-		[\"BOOLEAN\",16,1,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,16,0,null,null],\
-		[\"DATE\",91,29,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,91,1,null,null],\
-		[\"TIME\",92,18,\"'\",\"'\",null,2,false,3,true,false,false,null,3,3,92,2,null,null],\
-		[\"DATETIME\",93,29,\"'\",\"'\",null,2,false,3,true,false,false,null,3,3,9,3,null,null],\
-		[\"INTERVAL_YEAR\",101,7,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,101,0,null,null],\
-		[\"INTERVAL_MONTH\",102,7,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,102,0,null,null],\
-		[\"INTERVAL_DAY\",103,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,103,0,null,null],\
-		[\"INTERVAL_HOUR\",104,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,104,0,null,null],\
-		[\"INTERVAL_MINUTE\",105,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,105,0,null,null],\
-		[\"INTERVAL_SECOND\",106,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,106,0,null,null],\
-		[\"INTERVAL_YEAR_TO_MONTH\",107,7,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,107,0,null,null],\
-		[\"INTERVAL_DAY_TO_HOUR\",108,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,108,0,null,null],\
-		[\"INTERVAL_DAY_TO_MINUTE\",109,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,109,0,null,null],\
-		[\"INTERVAL_DAY_TO_SECOND\",110,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,110,0,null,null],\
-		[\"INTERVAL_HOUR_TO_MINUTE\",111,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,111,0,null,null],\
-		[\"INTERVAL_HOUR_TO_SECOND\",112,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,112,0,null,null],\
-		[\"INTERVAL_MINUTE_TO_SECOND\",113,23,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,113,0,null,null],\
-		[\"UNSUPPORTED\",1111,0,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,1111,0,null,null],\
-		[\"OBJECT\",2002,0,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,2002,0,null,null],\
-		[\"NESTED\",2002,0,\"'\",\"'\",null,2,false,3,true,false,false,null,null,null,2002,0,null,null]\
-	]\
+  \"columns\" : [\
+    {\"name\" : \"TYPE_NAME\", \"type\" : \"keyword\", \"display_size\" : 32766},\
+    {\"name\" : \"DATA_TYPE\", \"type\" : \"integer\", \"display_size\" : 11},\
+    {\"name\" : \"PRECISION\", \"type\" : \"integer\", \"display_size\" : 11},\
+    {\"name\" : \"LITERAL_PREFIX\", \"type\" : \"keyword\", \"display_size\" : 32766},\
+    {\"name\" : \"LITERAL_SUFFIX\", \"type\" : \"keyword\", \"display_size\" : 32766},\
+    {\"name\" : \"CREATE_PARAMS\", \"type\" : \"keyword\", \"display_size\" : 32766},\
+    {\"name\" : \"NULLABLE\", \"type\" : \"short\", \"display_size\" : 6},\
+    {\"name\" : \"CASE_SENSITIVE\", \"type\" : \"boolean\", \"display_size\" : 1},\
+    {\"name\" : \"SEARCHABLE\", \"type\" : \"short\", \"display_size\" : 6},\
+    {\"name\" : \"UNSIGNED_ATTRIBUTE\", \"type\" : \"boolean\", \"display_size\" : 1},\
+    {\"name\" : \"FIXED_PREC_SCALE\", \"type\" : \"boolean\", \"display_size\" : 1},\
+    {\"name\" : \"AUTO_INCREMENT\", \"type\" : \"boolean\", \"display_size\" : 1},\
+    {\"name\" : \"LOCAL_TYPE_NAME\", \"type\" : \"keyword\", \"display_size\" : 32766},\
+    {\"name\" : \"MINIMUM_SCALE\", \"type\" : \"short\", \"display_size\" : 6},\
+    {\"name\" : \"MAXIMUM_SCALE\", \"type\" : \"short\", \"display_size\" : 6},\
+    {\"name\" : \"SQL_DATA_TYPE\", \"type\" : \"integer\", \"display_size\" : 11},\
+    {\"name\" : \"SQL_DATETIME_SUB\", \"type\" : \"integer\", \"display_size\" : 11},\
+    {\"name\" : \"NUM_PREC_RADIX\", \"type\" : \"integer\", \"display_size\" : 11},\
+    {\"name\" : \"INTERVAL_PRECISION\", \"type\" : \"integer\", \"display_size\" : 11}\
+  ],\
+  \"rows\" : [\
+    [\"BYTE\", -6, 3, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 0, 0, -6, 0, 10, null],\
+    [\"LONG\", -5, 19, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 0, 0, -5, 0, 10, null],\
+    [\"BINARY\", -2, 2147483647, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, -2, 0, null, null],\
+    [\"NULL\", 0, 0, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 0, 0, null, null],\
+    [\"INTEGER\", 4, 10, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 0, 0, 4, 0, 10, null],\
+    [\"SHORT\", 5, 5, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 0, 0, 5, 0, 10, null],\
+    [\"HALF_FLOAT\", 6, 3, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 3, 3, 6, 0, 2, null],\
+    [\"FLOAT\", 7, 7, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 7, 7, 7, 0, 2, null],\
+    [\"DOUBLE\", 8, 15, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 15, 15, 8, 0, 2, null],\
+    [\"SCALED_FLOAT\", 8, 15, \"'\", \"'\", null, 2, false, 3, false, false, false, null, 15, 15, 8, 0, 2, null],\
+    [\"CONSTANT_KEYWORD\", 12, 32766, \"'\", \"'\", null, 2, true, 3, true, false, false, null, null, null, 12, 0, null, null],\
+    [\"IP\", 12, 45, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 12, 0, null, null],\
+    [\"KEYWORD\", 12, 32766, \"'\", \"'\", null, 2, true, 3, true, false, false, null, null, null, 12, 0, null, null],\
+    [\"TEXT\", 12, 2147483647, \"'\", \"'\", null, 2, true, 3, true, false, false, null, null, null, 12, 0, null, null],\
+    [\"BOOLEAN\", 16, 1, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 16, 0, null, null],\
+    [\"DATE\", 91, 29, \"'\", \"'\", null, 2, false, 3, true, false, false, null, 3, 3, 91, 0, null, null],\
+    [\"TIME\", 92, 18, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 92, 0, null, null],\
+    [\"DATETIME\", 93, 29, \"'\", \"'\", null, 2, false, 3, true, false, false, null, 3, 3, 9, 3, null, null],\
+    [\"INTERVAL_YEAR\", 101, 7, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 101, 0, null, null],\
+    [\"INTERVAL_MONTH\", 102, 7, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 102, 0, null, null],\
+    [\"INTERVAL_DAY\", 103, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 103, 0, null, null],\
+    [\"INTERVAL_HOUR\", 104, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 104, 0, null, null],\
+    [\"INTERVAL_MINUTE\", 105, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 105, 0, null, null],\
+    [\"INTERVAL_SECOND\", 106, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 106, 0, null, null],\
+    [\"INTERVAL_YEAR_TO_MONTH\", 107, 7, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 107, 0, null, null],\
+    [\"INTERVAL_DAY_TO_HOUR\", 108, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 108, 0, null, null],\
+    [\"INTERVAL_DAY_TO_MINUTE\", 109, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 109, 0, null, null],\
+    [\"INTERVAL_DAY_TO_SECOND\", 110, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 110, 0, null, null],\
+    [\"INTERVAL_HOUR_TO_MINUTE\", 111, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 111, 0, null, null],\
+    [\"INTERVAL_HOUR_TO_SECOND\", 112, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 112, 0, null, null],\
+    [\"INTERVAL_MINUTE_TO_SECOND\", 113, 23, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 113, 0, null, null],\
+    [\"GEO_POINT\", 114, 58, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 114, 0, null, null],\
+    [\"GEO_SHAPE\", 114, 2147483647, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 114, 0, null, null],\
+    [\"SHAPE\", 114, 2147483647, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 114, 0, null, null],\
+    [\"UNSUPPORTED\", 1111, 0, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 1111, 0, null, null],\
+    [\"NESTED\", 2002, 0, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 2002, 0, null, null],\
+    [\"OBJECT\", 2002, 0, \"'\", \"'\", null, 2, false, 3, true, false, false, null, null, null, 2002, 0, null, null] \
+  ]\
 }"
+
 
 /* minimal, valid connection string */
 #define CONNECT_STRING L"Driver=ElasticODBC;Packing=JSON;"
